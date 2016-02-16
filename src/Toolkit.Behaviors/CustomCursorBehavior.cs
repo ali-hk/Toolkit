ead6236125ff5874a6b1dd3234e12a2859910786
Ali Hosseini Khayat <alihk@live.com>
2016-02-15 16:15:41 -0800

    -Added CustomCursorBehavior -Added RightTapMenuAction

---
+++ src/Toolkit.Behaviors/CustomCursorBehavior.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+
+namespace Toolkit.Behaviors
+{
+    public class CustomCursorBehavior : Behavior<FrameworkElement>
+    {
+        public static readonly DependencyProperty CursorTypeProperty =
+            DependencyProperty.Register("CursorType", typeof(CoreCursorType), typeof(CustomCursorBehavior), new PropertyMetadata(CoreCursorType.Arrow));
+
+        public CoreCursorType CursorType
+        {
+            get { return (CoreCursorType)GetValue(CursorTypeProperty); }
+            set { SetValue(CursorTypeProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.PointerEntered += OnPointerEntered;
+            AssociatedObject.PointerExited += OnPointerExited;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PointerEntered -= OnPointerEntered;
+            AssociatedObject.PointerExited -= OnPointerExited;
+        }
+
+        private void OnPointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            Window.Current.CoreWindow.PointerCursor = new CoreCursor(CursorType, 0);
+        }
+
+        private void OnPointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            Window.Current.CoreWindow.PointerCursor = new CoreCursor(CoreCursorType.Arrow, 0);
+        }
+    }
+}
---
+++ src/Toolkit.TestApp/MenuItemViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolkit.Xaml.ContextMenu;
+using System.Windows.Input;
+
+namespace Toolkit.TestApp
+{
+    public class MenuItemViewModel : BindableBase, IContextMenuItem
+    {
+        public MenuItemViewModel(string title, ICommand command)
+        {
+            Title = title;
+            Command = command;
+        }
+
+        public ICommand Command { get; }
+
+        public string Title { get; }
+    }
+}
