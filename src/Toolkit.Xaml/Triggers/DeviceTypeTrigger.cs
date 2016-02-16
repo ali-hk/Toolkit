092e1cde32786f9ea616fcf4d7b582a1d1c41085
Ali Hosseini Khayat <alihk@live.com>
2016-02-16 00:13:38 -0800

    -Added DeviceTypeTrigger -Added ToggleableAdaptiveTrigger

---
+++ src/Toolkit.Xaml/Triggers/DeviceTypeTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolkit.Uwp;
+using Windows.UI.Xaml;
+
+namespace Toolkit.Xaml.Triggers
+{
+    public class DeviceTypeTrigger : StateTriggerBase
+    {
+        public static readonly DependencyProperty ActiveDeviceTypeProperty = DependencyProperty.Register("ActiveDeviceType", typeof(DeviceType), typeof(DeviceTypeTrigger), new PropertyMetadata(null, OnTriggerPropertyChanged));
+
+        public DeviceTypeTrigger()
+        {
+        }
+
+        public DeviceType ActiveDeviceType
+        {
+            get
+            {
+                return (DeviceType)GetValue(ActiveDeviceTypeProperty);
+            }
+
+            set
+            {
+                SetValue(ActiveDeviceTypeProperty, value);
+            }
+        }
+
+        private static void OnTriggerPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var trig = d as DeviceTypeTrigger;
+            trig.UpdateTrigger();
+        }
+
+        private void UpdateTrigger()
+        {
+            var activeDeviceType = ActiveDeviceType;
+            SetActive(DeviceTypeHelper.GetDeviceType() == activeDeviceType);
+        }
+    }
+}
