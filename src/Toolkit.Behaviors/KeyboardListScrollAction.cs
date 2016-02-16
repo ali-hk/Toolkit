d4b66fe2c599e77aa05b5f1230019b5f03b2f809
Ali Hosseini Khayat <alihk@live.com>
2016-02-15 17:19:03 -0800

    -Added KeyboardListScrollAction -Added PagedScrollAction

---
+++ src/Toolkit.Behaviors/KeyboardListScrollAction.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolkit.Xaml.VisualTree;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Input;
+
+namespace Toolkit.Behaviors
+{
+    public class KeyboardListScrollAction : DependencyObject, IAction
+    {
+        public static readonly DependencyProperty HorizontalPageSizeProperty =
+            DependencyProperty.Register("HorizontalPageSize", typeof(int), typeof(KeyboardListScrollAction), new PropertyMetadata(0));
+
+        public static readonly DependencyProperty VerticalPageSizeProperty =
+            DependencyProperty.Register("VerticalPageSize", typeof(int), typeof(KeyboardListScrollAction), new PropertyMetadata(0));
+
+        public static readonly DependencyProperty ScrollDirectionDelegateProperty =
+            DependencyProperty.Register("ScrollDirectionDelegate", typeof(Func<VirtualKey, ScrollDirection>), typeof(KeyboardListScrollAction), new PropertyMetadata(null));
+
+        public int HorizontalPageSize
+        {
+            get { return (int)GetValue(HorizontalPageSizeProperty); }
+            set { SetValue(HorizontalPageSizeProperty, value); }
+        }
+
+        public int VerticalPageSize
+        {
+            get { return (int)GetValue(VerticalPageSizeProperty); }
+            set { SetValue(VerticalPageSizeProperty, value); }
+        }
+
+        public Func<VirtualKey, ScrollDirection> ScrollDirectionDelegate
+        {
+            get { return (Func<VirtualKey, ScrollDirection>)GetValue(ScrollDirectionDelegateProperty); }
+            set { SetValue(ScrollDirectionDelegateProperty, value); }
+        }
+
+        public object Execute(object sender, object parameter)
+        {
+            var eventArgs = parameter as KeyRoutedEventArgs;
+
+            var listViewBase = sender as ListViewBase;
+            if (listViewBase == null)
+            {
+                throw new ArgumentException($"{nameof(KeyboardListScrollAction)} can only be used with ListViewBase");
+            }
+
+            if (ScrollDirectionDelegate == null)
+            {
+                throw new ArgumentNullException(nameof(ScrollDirectionDelegate));
+            }
+
+            var horizontalPageSize = HorizontalPageSize;
+            var verticalPageSize = VerticalPageSize;
+
+            var scrollableSize = horizontalPageSize * verticalPageSize;
+
+            var currentItem = eventArgs.OriginalSource as SelectorItem;
+            if (currentItem == null)
+            {
+                currentItem = VisualTreeUtilities.GetFirstAncestorOfType<SelectorItem>(eventArgs.OriginalSource as DependencyObject);
+            }
+
+            if (currentItem == null)
+            {
+                throw new ArgumentException($"No SelectorItem found in visual tree for {eventArgs.OriginalSource.ToString()}");
+            }
+
+            var currentIndex = listViewBase.IndexFromContainer(currentItem);
+
+            if (currentIndex < 0)
+            {
+                Debug.Assert(currentIndex >= 0, "Current item hasn't been realized yet");
+                return null;
+            }
+
+            var direction = ScrollDirectionDelegate?.Invoke(eventArgs.Key);
+            if (direction == ScrollDirection.None)
+            {
+                return null;
+            }
+
+            var targetIndex = currentIndex + (direction == ScrollDirection.Up || direction == ScrollDirection.Left ? -scrollableSize : scrollableSize);
+
+            var max = listViewBase.Items.Count - 1;
+            targetIndex = Math.Min(max, Math.Max(0, targetIndex));
+
+            if (direction == ScrollDirection.Up || direction == ScrollDirection.Left)
+            {
+                for (var i = targetIndex; i < currentIndex; i++)
+                {
+                    if (FocusIndex(listViewBase, i))
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (var i = targetIndex; i > currentIndex; i--)
+                {
+                    if (FocusIndex(listViewBase, i))
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static bool FocusIndex(ListViewBase listViewBase, int i)
+        {
+            var newContainer = listViewBase.ContainerFromIndex(i) as Control;
+            if (newContainer == null)
+            {
+                return false;
+            }
+
+            newContainer.Focus(FocusState.Keyboard);
+            listViewBase.ScrollIntoView(newContainer);
+            return true;
+        }
+    }
+}
---
+++ src/Toolkit.Behaviors/PagedScrollAction.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolkit.Xaml.VisualTree;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Toolkit.Behaviors
+{
+    /// <summary>
+    /// Action to allow a button click or similar event to cause a scrollable target to scroll in a given direction
+    /// </summary>
+    public class PagedScrollAction : DependencyObject, IAction
+    {
+        public static readonly DependencyProperty ScrollTargetProperty =
+            DependencyProperty.Register("ScrollTarget", typeof(DependencyObject), typeof(PagedScrollAction), new PropertyMetadata(null, OnScrollTargetChanged));
+
+        public static readonly DependencyProperty ScrollDirectionProperty =
+            DependencyProperty.Register("ScrollDirection", typeof(ScrollDirection), typeof(PagedScrollAction), new PropertyMetadata(ScrollDirection.None));
+
+        private ScrollViewer _scrollViewer = null;
+
+        public DependencyObject ScrollTarget
+        {
+            get { return (DependencyObject)GetValue(ScrollTargetProperty); }
+            set { SetValue(ScrollTargetProperty, value); }
+        }
+
+        public ScrollDirection ScrollDirection
+        {
+            get { return (ScrollDirection)GetValue(ScrollDirectionProperty); }
+            set { SetValue(ScrollDirectionProperty, value); }
+        }
+
+        public object Execute(object sender, object parameter)
+        {
+            Debug.Assert(ScrollDirection != ScrollDirection.None, $"{nameof(ScrollDirection)} is set to {nameof(ScrollDirection.None)}. This behavior isn't doing anything");
+
+            if (ScrollTarget != null && ScrollDirection != ScrollDirection.None)
+            {
+                if (_scrollViewer == null)
+                {
+                    _scrollViewer = FindScrollViewer();
+                }
+
+                if (_scrollViewer != null)
+                {
+                    if (ScrollDirection == ScrollDirection.Left || ScrollDirection == ScrollDirection.Right)
+                    {
+                        ScrollPageHorizontal(_scrollViewer, ScrollDirection);
+                    }
+                    else
+                    {
+                        ScrollPageVertical(_scrollViewer, ScrollDirection);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static void ScrollPageHorizontal(ScrollViewer scrollViewer, ScrollDirection direction)
+        {
+            if (scrollViewer == null || direction == ScrollDirection.None)
+            {
+                return;
+            }
+
+            double actualWidth = scrollViewer.ActualWidth;
+            double currentOffset = scrollViewer.HorizontalOffset;
+            double newWidth = 0;
+
+            if (direction == ScrollDirection.Left)
+            {
+                newWidth = currentOffset - actualWidth;
+            }
+            else if (direction == ScrollDirection.Right)
+            {
+                newWidth = currentOffset + actualWidth;
+            }
+
+            if (newWidth > scrollViewer.ExtentWidth)
+            {
+                newWidth = currentOffset;
+            }
+
+            if (newWidth < 0)
+            {
+                newWidth = 0;
+            }
+
+            scrollViewer.ChangeView(newWidth, null, null);
+        }
+
+        private static void ScrollPageVertical(ScrollViewer scrollViewer, ScrollDirection direction)
+        {
+            if (scrollViewer == null || direction == ScrollDirection.None)
+            {
+                return;
+            }
+
+            double actualHeight = scrollViewer.ActualHeight;
+            double currentOffset = scrollViewer.VerticalOffset;
+            double newHeight = 0;
+
+            if (direction == ScrollDirection.Up)
+            {
+                newHeight = currentOffset - actualHeight;
+            }
+            else if (direction == ScrollDirection.Down)
+            {
+                newHeight = currentOffset + actualHeight;
+            }
+
+            if (newHeight > scrollViewer.ExtentHeight)
+            {
+                newHeight = currentOffset;
+            }
+
+            if (newHeight < 0)
+            {
+                newHeight = 0;
+            }
+
+            scrollViewer.ChangeView(null, newHeight, null);
+        }
+
+        private static void OnScrollTargetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var pagedScrollAction = d as PagedScrollAction;
+            if (pagedScrollAction != null)
+            {
+                pagedScrollAction._scrollViewer = pagedScrollAction.FindScrollViewer();
+            }
+        }
+
+        private ScrollViewer FindScrollViewer()
+        {
+            if (ScrollTarget != null)
+            {
+                return ScrollTarget.GetVisualChild<ScrollViewer>();
+            }
+
+            return null;
+        }
+    }
+}
