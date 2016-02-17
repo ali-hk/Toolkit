a469875561b31973be769ed0d7072986517f3081
Ali Hosseini Khayat <ali.hosseini.khayat@live.com>
2016-02-16 21:46:31 -0800

    -Added some ViewModels to use -Removed app reference from UnitTest

---
+++ src/Toolkit.TestApp/ViewModels/GoaltenderViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Prism.Windows.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolkit.TestApp.ViewModels
+{
+    public class GoaltenderViewModel : ViewModelBase
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Number { get; set; }
+
+        public DateTime Born { get; set; }
+
+        public string NationalTeam { get; set; }
+
+        public int GamesPlayed { get; set; }
+
+        public int Wins { get; set; }
+
+        public int Losses { get; set; }
+
+        public int Ties { get; set; }
+
+        public int Minutes { get; set; }
+
+        public int GoalsAgainst { get; set; }
+
+        public int Shootouts { get; set; }
+
+        public float GoalsAgainstAverage { get; set; }
+
+        public float SavePercentage { get; set; }
+
+        public IReadOnlyCollection<TeamViewModel> Teams { get; set; }
+    }
+}
---
+++ src/Toolkit.TestApp/ViewModels/HockeyPlayerViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Prism.Windows.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolkit.TestApp.ViewModels
+{
+    public class HockeyPlayerViewModel : ViewModelBase
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Number { get; set; }
+
+        public DateTime Born { get; set; }
+
+        public string NationalTeam { get; set; }
+
+        public HockeyPosition Position { get; set; }
+
+        public ShotType Shot { get; set; }
+
+        public int GamesPlayed { get; set; }
+
+        public int Goals { get; set; }
+
+        public int Assists { get; set; }
+
+        public int Points { get; set; }
+
+        public int PlusMinus { get; set; }
+
+        public int GameWinningGoals { get; set; }
+
+        public int PenaltyMinutes { get; set; }
+
+        public IReadOnlyCollection<TeamViewModel> Teams { get; set; }
+    }
+}
