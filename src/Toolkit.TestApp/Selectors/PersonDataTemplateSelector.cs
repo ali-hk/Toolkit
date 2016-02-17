0f25c9f4307f9a1ddd6ee64bcef23233f2442b62
Ali Hosseini Khayat <ali.hosseini.khayat@live.com>
2016-02-16 22:26:03 -0800

    -Added PersonDataTemplateSelector -Added code to populate People in MainPageViewModel

---
+++ src/Toolkit.TestApp/Selectors/PersonDataTemplateSelector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolkit.TestApp.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Toolkit.TestApp.Selectors
+{
+    public class PersonDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate HockeyPlayerDataTemplate { get; set; }
+
+        public DataTemplate GoaltenderDataTemplate { get; set; }
+
+        public DataTemplate DriverDataTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            if (item is HockeyPlayerViewModel)
+            {
+                return HockeyPlayerDataTemplate;
+            }
+            else if (item is GoaltenderViewModel)
+            {
+                return GoaltenderDataTemplate;
+            }
+            else if (item is DriverViewModel)
+            {
+                return DriverDataTemplate;
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
