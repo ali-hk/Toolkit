f54a2b2bc87b3ad4f4aea0f61592a4e8132601cc
Ali Hosseini Khayat <alihk@live.com>
2016-02-18 08:13:00 -0800

    Adding DataTemplateSelectorBehavior

---
+++ src/Toolkit.Behaviors/DataTemplateMapping.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+
+namespace Toolkit.Behaviors
+{
+    public class DataTemplateMapping : DependencyObject
+    {
+        // TODO: Add a DP for cache length
+        public static readonly DependencyProperty TypeNameProperty =
+            DependencyProperty.Register("TypeName", typeof(string), typeof(DataTemplateMapping), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty TemplateProperty =
+            DependencyProperty.Register("Template", typeof(DataTemplate), typeof(DataTemplateMapping), new PropertyMetadata(null));
+
+        public string TypeName
+        {
+            get { return (string)GetValue(TypeNameProperty); }
+            set { SetValue(TypeNameProperty, value); }
+        }
+
+        public DataTemplate Template
+        {
+            get { return (DataTemplate)GetValue(TemplateProperty); }
+            set { SetValue(TemplateProperty, value); }
+        }
+    }
+}
---
+++ src/Toolkit.Behaviors/DataTemplateSelectorBehavior.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace Toolkit.Behaviors
+{
+    public class DataTemplateSelectorBehavior : Behavior<ListViewBase>
+    {
+        public static readonly DependencyProperty MappingProperty =
+            DependencyProperty.Register("Mapping", typeof(string), typeof(DataTemplateSelectorBehavior), new PropertyMetadata(null, new PropertyChangedCallback(OnMappingChanged)));
+
+        private Dictionary<string, DataTemplate> _typeToTemplateMapping;
+        private Dictionary<string, Queue<SelectorItem>> _typeToItemQueueMapping;
+
+        public DataTemplateSelectorBehavior()
+        {
+            Mappings = new DataTemplateMappingCollection();
+            _typeToTemplateMapping = new Dictionary<string, DataTemplate>();
+            _typeToItemQueueMapping = new Dictionary<string, Queue<SelectorItem>>();
+            Mappings.ItemAdded += Mappings_ItemAdded;
+
+            foreach (var item in Mappings)
+            {
+                AddTypeMapping(item);
+            }
+        }
+
+        public DataTemplateMappingCollection Mappings { get; }
+
+        public string Mapping
+        {
+            get { return (string)GetValue(MappingProperty); }
+            set { SetValue(MappingProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.ChoosingItemContainer += OnChoosingItemContainer;
+            ProcessMappings(Mapping);
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.ChoosingItemContainer -= OnChoosingItemContainer;
+        }
+
+        private static void OnMappingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var behavior = d as DataTemplateSelectorBehavior;
+            behavior.ProcessMappings(e.NewValue as string);
+        }
+
+        private void Mappings_ItemAdded(object sender, DataTemplateMapping e)
+        {
+            AddTypeMapping(e);
+        }
+
+        private void AddTypeMapping(DataTemplateMapping mapping)
+        {
+            _typeToTemplateMapping.Add(mapping.TypeName, mapping.Template);
+            _typeToItemQueueMapping.Add(mapping.TypeName, new Queue<SelectorItem>());
+            var queue = _typeToItemQueueMapping[mapping.TypeName];
+            for (int i = 0; i < 5; i++)
+            {
+                SelectorItem item = null;
+                if (AssociatedObject is GridView)
+                {
+                    item = new GridViewItem();
+                }
+                else
+                {
+                    item = new ListViewItem();
+                }
+
+                item.ContentTemplate = _typeToTemplateMapping[mapping.TypeName];
+                item.Tag = mapping.TypeName;
+                queue.Enqueue(item);
+            }
+        }
+
+        private void ProcessMappings(string mapping)
+        {
+            if (AssociatedObject == null)
+            {
+                return;
+            }
+
+            var mappings = mapping.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in mappings)
+            {
+                var parts = item.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                AddTypeMapping(new DataTemplateMapping { TypeName = parts[0], Template = AssociatedObject.Resources[parts[1]] as DataTemplate });
+            }
+        }
+
+        private void OnChoosingItemContainer(ListViewBase sender, ChoosingItemContainerEventArgs args)
+        {
+            var typeName = args.Item.GetType().Name;
+
+            // args.ItemContainer is used to indicate whether the ListView is proposing an
+            // ItemContainer (ListViewItem) to use. If args.Itemcontainer, then there was a
+            // recycled ItemContainer available to be reused.
+            if (args.ItemContainer != null)
+            {
+                // The Tag is being used to determine whether this is a special file or
+                // a simple file.
+                if (!args.ItemContainer.Tag.Equals(typeName))
+                {
+                    // The ItemContainer's datatemplate does not match the needed
+                    // datatemplate.
+                    args.ItemContainer = null;
+                }
+            }
+
+            if (args.ItemContainer == null && _typeToTemplateMapping.ContainsKey(typeName))
+            {
+                // TODO: retrieve this safely
+                var relevantQueue = _typeToItemQueueMapping[typeName];
+
+                // See if we can fetch from the correct list.
+                if (relevantQueue.Count > 0)
+                {
+                    args.ItemContainer = relevantQueue.Dequeue();
+                }
+                else
+                {
+                    // There aren't any (recycled) ItemContainers available. So a new one
+                    // needs to be created.
+                    SelectorItem item = null;
+                    if (sender is GridView)
+                    {
+                        item = new GridViewItem();
+                    }
+                    else
+                    {
+                        item = new ListViewItem();
+                    }
+
+                    item.ContentTemplate = _typeToTemplateMapping[typeName];
+                    item.Tag = typeName;
+                    args.ItemContainer = item;
+                }
+            }
+
+            args.IsContainerPrepared = true;
+        }
+    }
+}
