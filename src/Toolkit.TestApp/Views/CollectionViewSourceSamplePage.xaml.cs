ce864516825a0932726c4704ed4806300fc32876
Ali Hosseini Khayat <alihk@live.com>
2016-02-20 23:13:43 -0800

    -Added CollectionViewSource Sample

---
+++ src/Toolkit.TestApp/Views/CollectionViewSourceSamplePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Toolkit.Prism.Mvvm;
+using Toolkit.TestApp.PageViewModels;
+using Toolkit.TestApp.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+namespace Toolkit.TestApp.Views
+{
+    public sealed partial class CollectionViewSourceSamplePage : MvvmPage
+    {
+        public CollectionViewSourceSamplePage()
+        {
+            InitializeComponent();
+        }
+
+        public CollectionViewSourceSamplePageViewModel ConcreteDataContext
+        {
+            get
+            {
+                return DataContext as CollectionViewSourceSamplePageViewModel;
+            }
+        }
+    }
+}
