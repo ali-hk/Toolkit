abec6aaac46b75a10d6ce104167dd4a1dadf7c0b
Ali Hosseini Khayat <alihk@live.com>
2016-04-19 03:16:40 -0700

    Adding Toolkit template pack, including: -ItemTemplate for Page, PageVM -ProjectTemplate for Blank, Hamburger Menu apps -Snippets

---
+++ src/ToolkitTemplatePack/ProjectTemplates/PrismToolkitHamburgerMenuApp/Views/MenuView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using $safeprojectname$.PageViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Toolkit.Prism.Mvvm;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// TODO: Sort usings and remove this Code Analysis suppression
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.Analyzers", "SA1210", Justification = "Template can't sort project's using")]
+
+namespace $safeprojectname$.Views
+{
+    public sealed partial class MenuView : MvvmUserControl
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+
+        public MenuViewModel ConcreteDataContext
+        {
+            get
+            {
+                return DataContext as MenuViewModel;
+            }
+        }
+    }
+}
