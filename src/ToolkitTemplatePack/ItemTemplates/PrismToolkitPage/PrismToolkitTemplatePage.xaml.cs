d8dfd04eb7cab13db647894b3faa98406473f196
Ali Hosseini Khayat <ali.hosseini.khayat@live.com>
2016-04-19 03:16:40 -0700

    Adding Toolkit template pack, including: -ItemTemplate for Page, PageVM -ProjectTemplate for Blank, Hamburger Menu apps -Snippets

---
+++ src/ToolkitTemplatePack/ItemTemplates/PrismToolkitPage/PrismToolkitTemplatePage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Toolkit.Prism.Mvvm;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+namespace $rootnamespace$
+{
+    public sealed partial class $safeitemname$ : MvvmPage
+    {
+        public $safeitemname$()
+        {
+            InitializeComponent();
+        }
+
+        public $safeitemname$ViewModel ConcreteDataContext
+        {
+            get
+            {
+                return DataContext as $safeitemname$ViewModel;
+            }
+        }
+    }
+}
---
+++ src/ToolkitTemplatePack/ProjectTemplates/PrismToolkitApp/Views/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using $safeprojectname$.PageViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Toolkit.Prism.Mvvm;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// TODO: Sort usings and remove this Code Analysis suppression
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.Analyzers", "SA1210", Justification = "Template can't sort project's using")]
+
+namespace $safeprojectname$.Views
+{
+    public sealed partial class MainPage : MvvmPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public MainPageViewModel ConcreteDataContext
+        {
+            get
+            {
+                return DataContext as MainPageViewModel;
+            }
+        }
+    }
+}
