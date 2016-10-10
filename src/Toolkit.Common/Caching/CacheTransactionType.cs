0ab9e6f0e45b4eef463157fa6a199f377a25a4b4
ali-hk <ali-hk>
2016-10-09 17:18:33 -0700

    -Adding InMemoryCache -Adding ScopedReaderWriterLock -Adding IUpdatable

---
+++ src/Toolkit.Common/Caching/CacheTransactionType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolkit.Common.Caching
+{
+    [Flags]
+    public enum CacheTransactionType
+    {
+        // None is intentionally not defined as 0
+        // because we want None to be mutually exclusive
+        // of the other values in this context. In other words,
+        // None is an explicit state here, not the lack of any state
+        Indeterminate = 0,
+        None = 1,
+        Add = 2,
+        Remove = 4,
+        Update = 8
+    }
+}
---
+++ src/Toolkit.Common/Caching/IUpdatable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolkit.Common.Caching
+{
+    public interface IUpdatable<T>
+    {
+        /// <summary>
+        /// Updates the current instance with data from <paramref name="updateSource"/>
+        /// </summary>
+        /// <param name="updateSource">Object with updated data</param>
+        /// <returns>True if there was a change, false otherwise</returns>
+        bool Update(T updateSource);
+    }
+}
