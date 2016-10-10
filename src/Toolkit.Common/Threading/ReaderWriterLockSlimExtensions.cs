16e8fb4f56a59e77819a1ecc4fd1382bb955a9d3
Ali Hosseini Khayat <ali.hosseini.khayat@live.com>
2016-10-09 17:18:33 -0700

    -Adding InMemoryCache -Adding ScopedReaderWriterLock -Adding IUpdatable

---
+++ src/Toolkit.Common/Threading/ReaderWriterLockSlimExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Toolkit.Common.Threading
+{
+    public static class ReaderWriterLockSlimExtensions
+    {
+        public static ScopedReaderWriterLock ScopedReadLock(this ReaderWriterLockSlim readerWriterLock)
+        {
+            return new ScopedReaderWriterLock(readerWriterLock);
+        }
+
+        public static ScopedReaderWriterLock ScopedWriteLock(this ReaderWriterLockSlim readerWriterLock)
+        {
+            return new ScopedReaderWriterLock(readerWriterLock, readOnly: false);
+        }
+    }
+}
